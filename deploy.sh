8e196edeec0f0963ce32d52a0b8924766ee4b2b5
Thijs Baaijen <Thijss@users.noreply.github.com>
2023-04-21 19:50:35 +0200

    alter deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,14 @@
 # Note: This script assumes you have a lambda function on AWS with the same name as the current directory
 # You can alter "${PWD##*/}" to a fixed name if this is not the case
+
+# Zip
+cd .venv/lib/python3.10/site-packages
+zip -q -r ../../../../package.zip .
+cd ../../../../
+zip -q -r -g package.zip app
+
+# Deploy
 aws lambda update-function-code --function-name "${PWD##*/}" --zip-file fileb://package.zip
+
+# Cleanup
+rm package.zip
--- lambda_zip.sh
+++
@@ -1,4 +0,0 @@
-cd .venv/lib/python3.10/site-packages
-zip -q -r ../../../../package.zip .
-cd ../../../../
-zip -q -r -g package.zip app
